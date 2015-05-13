b51ff2ea8798664cc102356ff92a610c9a3fc7fc
Katie McLaughlin <katie@glasnt.com>
2015-05-13 15:48:27 +1000

    Automatic Rubocop Fixes

--- bin/alert_threshold_recommendations.rb
+++ bin/alert_threshold_recommendations.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-$:.push(File.expand_path(File.join(__FILE__, '..', '..', 'lib')))
+$LOAD_PATH.push(File.expand_path(File.join(__FILE__, '..', '..', 'lib')))
 
 require 'time'
 require 'methadone'
@@ -10,7 +10,7 @@ include Methadone::Main
 include Methadone::CLILogging
 
 main do
-  raise "Sort by must be one of frequency, threshold, or incident_key" unless %w(frequency threshold incident_key).include?(options[:s])
+  raise 'Sort by must be one of frequency, threshold, or incident_key' unless %w(frequency threshold incident_key).include?(options[:s])
   influxdb = Influx::Db.new
   opts = {
     :start_date  => "#{options[:t]} ago",
@@ -34,12 +34,12 @@ end
 
 use_log_level_option
 
-description "Calculates suggested alert thresholds for removing X% of alerts over a given time period"
+description 'Calculates suggested alert thresholds for removing X% of alerts over a given time period'
 
-on("-p percent", "--percent-to-remove", "The percent of alerts to remove")
-on("-t duration", "--time-period", "The amount of time we should look over")
-on("-m more-than", "--more-than", "Only return results that have more than Y occurences")
-on("-r recover-within", "--recover-within", "Only return results that recover within this amount of time")
-on("-s sort-by", "--sort-by", "The field we should sort the returned data by - frequency, threshold, or incident_key")
+on('-p percent', '--percent-to-remove', 'The percent of alerts to remove')
+on('-t duration', '--time-period', 'The amount of time we should look over')
+on('-m more-than', '--more-than', 'Only return results that have more than Y occurences')
+on('-r recover-within', '--recover-within', 'Only return results that recover within this amount of time')
+on('-s sort-by', '--sort-by', 'The field we should sort the returned data by - frequency, threshold, or incident_key')
 
 go!
