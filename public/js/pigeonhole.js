efc7d07928748e7eb4566c8483281719bc91ec10
Paul O'Connor <paul@poconnor.me>
2015-06-02 15:57:10 +0100

    Bump jquery and remove date helper

--- public/js/pigeonhole.js
+++ public/js/pigeonhole.js
@@ -1,22 +1,21 @@
 $(function(){
     
     // Capture 'load' button click
-    $("#reload").click(function() { 
-       loadFromDates()
-    })
+    $("#reload").click(function() {
+       loadFromDates();
+    });
 
     // Capture enter keypress on search field
-    $("#search").keyup(function(ev) { 
-        if (ev.which === 13) { 
-           loadFromDates()
+    $("#search").keyup(function(ev) {
+        if (ev.which === 13) {
+           loadFromDates();
         }
-
-    })
+    });
 
     $("#incidents-table").tablesorter({
         textExtraction: {
             4 : function(node, table, cellIndex) {
-                return $(node).find('option:selected').text()
+                return $(node).find('option:selected').text();
             }
         }
     });
@@ -41,13 +40,13 @@ $(function(){
 });
 
 
-function loadFromDates() { 
+function loadFromDates() {
     var start_date = $('#start_date')[0].value;
     var end_date   = $('#end_date')[0].value;
-    var search = ""
-    if ( $('#search')[0].value) { 
-      search = "?search="+ $('#search')[0].value
-    } 
+    var search = "";
+    if ( $('#search')[0].value) {
+      search = "?search="+ $('#search')[0].value;
+    }
     window.location.href = '../' + start_date + '/' + end_date + search;
-};
+}
 
